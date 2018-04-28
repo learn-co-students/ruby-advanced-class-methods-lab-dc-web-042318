5f0872519fb5c889a327da302d14a717dfda884b
Adam Cohn <adamcohn88@gmail.com>
2018-04-28 09:29:27 -0400

    song by naem

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,32 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  
   def self.all
     @@all
   end
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    @@all << self.new
+    @@all.last
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    @@all.last
+  end
+
+  def self.create_by_name(name)
+    Song.new(name)
   end
 
 end
