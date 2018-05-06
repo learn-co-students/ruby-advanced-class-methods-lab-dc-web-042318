ba88bcbe69b303b84761ffcc343ef1430045e090
github username <github email address>
2018-05-05 21:35:13 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,8 @@ class Song
   end
 
   def self.find_or_create_by_name(string)
-     self.find_by_name(string).nil? ? self.create_by_name(string) : self.find_by_name(string)
+     self.find_by_name (string) || self.create_by_name (string)
+     #googled# self.find_by_name(string).nil? ? self.create_by_name(string) : self.find_by_name(string)
   end
 
   def self.alphabetical
